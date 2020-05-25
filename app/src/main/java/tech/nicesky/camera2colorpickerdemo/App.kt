f9458c1a01e0543b73f23c2f11016f806fd9cc50
fairytale110 <mobile2017@qq.com>
2020-05-25 15:49:49 +0800

    init

---
+++ app/src/main/java/tech/nicesky/camera2colorpickerdemo/App.kt
@@ -0,0 +1,20 @@
+package tech.nicesky.camera2colorpickerdemo
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+    companion object{
+        private lateinit var instance: App
+
+        @Synchronized
+        fun context(): App{
+           return instance
+        }
+    }
+}
